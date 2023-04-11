3bdb51122ee87dd96d1106dbb923bf5583809dd9
lando <landonroha@gmail.com>
2023-04-11 16:53:06 -0700

    add tests

--- lulutechnical/lulutechnicalTests/lulutechnicalTests.swift
+++ lulutechnical/lulutechnicalTests/lulutechnicalTests.swift
@@ -10,6 +10,13 @@ import XCTest
 
 final class lulutechnicalTests: XCTestCase {
 
+    let userDefaultsSuiteName = "TestDefaults"
+
+    override func setUp() {
+        super.setUp()
+        UserDefaults().removePersistentDomain(forName: userDefaultsSuiteName)
+    }
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -17,20 +24,46 @@ final class lulutechnicalTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+    
+    func testShort() async throws {
+        let viewModel = ViewModel()
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        do {
+            _ = try await viewModel.add("1")
+        } catch {
+            XCTAssertEqual(error as! ItemVerificationError, ItemVerificationError.tooShort)
         }
     }
+    
+    func testLong() async throws {
+        let viewModel = ViewModel()
 
+        do {
+            _ = try await viewModel.add("123456789012345678900")
+        } catch {
+            XCTAssertEqual(error as! ItemVerificationError, ItemVerificationError.tooLong)
+        }
+    }
+    
+    func testDuplicate() async throws {
+        let viewModel = ViewModel()
+        
+        do {
+            let _ = try await viewModel.add("123")
+            let _ = try await viewModel.add("123")
+        } catch {
+            XCTAssertEqual(error as! ItemVerificationError, ItemVerificationError.duplicate)
+        }
+    }
+    
+    func testSuccess() async throws {
+        let viewModel = ViewModel()
+        
+        do {
+            let result = try await viewModel.add("12345678901234567890")
+            XCTAssertNotNil(result)
+        } catch {
+            XCTAssertNil(error)
+        }
+    }
 }
