04e607d4a7cd7136da72b5a16880cd5799efaf82
lando <landonroha@gmail.com>
2023-04-11 11:05:44 -0700

    add basic ui

--- lulutechnical/lulutechnical/ViewController.swift
+++ lulutechnical/lulutechnical/ViewController.swift
@@ -8,12 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    lazy var addButton: UIBarButtonItem = {
+        let button = UIBarButtonItem()
+        button.title = "hey"
+        button.action = #selector(barButtonAction)
+        button.target = self
+        return button
+    }()
+    
+    lazy var segmentedControl: UISegmentedControl = {
+        let control = UISegmentedControl(items: ["hey", "hi"])
+        return control
+    }()
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        return tableView
+    }()
+    
+    private func addSegmentedControl() {
+        view.addSubview(segmentedControl)
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+        segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+    }
+    
+    private func addTableView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 0).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "hi"
+        
+        navigationItem.setRightBarButton(addButton, animated: false)
+        addSegmentedControl()
+        addTableView()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @objc func barButtonAction() {
+       print("Button pressed")
+    }
 }
-
